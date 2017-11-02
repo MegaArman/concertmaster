2ce42a15820d65cdbd5624a52744675c90b05675
megaarman <arman1@vt.edu>
2017-11-02 13:35:49 -0400

    midiNmuToNoteString added and tested

--- tests/concertmaster.spec.js
+++ tests/concertmaster.spec.js
@@ -4,11 +4,20 @@ const cm = require("../lib/Concertmaster");
 
 test("noteToMidi", (t) =>
 {
-  t.deepEqual(cm.noteToMidi("C0"), 0, "min");
-  t.deepEqual(cm.noteToMidi("B#9"), 120, "max");
-  t.deepEqual(cm.noteToMidi("G#5"), 68, "sharp");
-  t.deepEqual(cm.noteToMidi("Gb5"), 66, "flat");
-  t.deepEqual(cm.noteToMidi("Cb0"), false, "too low");
+  t.deepEqual(cm.noteStringToMidiNum("C0"), 0, "min");
+  t.deepEqual(cm.noteStringToMidiNum("B#9"), 120, "max");
+  t.deepEqual(cm.noteStringToMidiNum("G#5"), 68, "sharp");
+  t.deepEqual(cm.noteStringToMidiNum("Gb5"), 66, "flat");
+  t.deepEqual(cm.noteStringToMidiNum("Cb0"), false, "too low");
+  t.end();
+});
+
+test("midiNumToNoteString", (t) =>
+{
+  t.deepEqual(cm.midiNumToNoteString(0), "C0", "min");
+  t.deepEqual(cm.midiNumToNoteString(119), "B9", "max");
+  t.deepEqual(cm.midiNumToNoteString(66), "F#5", "accidental");
+  t.deepEqual(cm.midiNumToNoteString(-1), false, "too low");
   t.end();
 });
 
